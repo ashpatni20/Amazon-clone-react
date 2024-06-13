39e3bf76ff9b89c69528c821d8b843c565fb3b7a
ashpatni20 <ash.patni20@gmail.com>
2024-06-13 21:46:55 +0530

    button

--- src/pages/SectionProducts.jsx
+++ src/pages/SectionProducts.jsx
@@ -7,15 +7,9 @@ import Header from "../components/Header";
 import useFetch from "../utils/useProduct";
 import Loading from "../components/Loading";
 
-
-const SectionProducts = () => {
-  
+const SectionProducts = () => { 
   const {products, setDetailProduct, setProduct, searchVal, val} = useContext(ProductContext);
-
-  console.log(val)
-
   const [data, loading, error] = useFetch(searchVal || val)
-  
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -23,7 +17,7 @@ const SectionProducts = () => {
       setProduct(data)
     }
     
-  }, [searchVal, data])
+  }, [searchVal, data]) 
 
   if(!products){
     return
@@ -41,13 +35,63 @@ const SectionProducts = () => {
       </>
     )
   }
+
+  console.log(products)
+
+
+  const handleSortByName = () => {
+    let arr = [...products];
+
+    let update = arr.sort((a, b) => {
+        return a.product_title.localeCompare(b.product_title);
+    });
+
+    // console.log(update);
+    setProduct(update);
+}
+
+
+  const handleSortByRating = () => {
+    let arr = [...products];
+
+    let update = arr.sort((a, b) => {
+        return b.product_star_rating.localeCompare(a.product_star_rating);
+    });
+
+    setProduct(update);
+  }
+
+  const handleSortByPrice = () => {
+    let arr = [...products];
+
+    let update = arr.sort((a, b) => {
+        if (a.unit_price && b.unit_price) {
+            let priceA = parseFloat(a.unit_price.replace('$', ''));
+            let priceB = parseFloat(b.unit_price.replace('$', ''));
+
+            return priceA - priceB;
+        } else {
+            return 0;
+        }
+    });
+
+    console.log(update);
+}
+
+
   return (
     <>
     {/* <Navbar/> */}
     <Header />
+    <>
+    <div className="ml-[10%] flex gap-3">
+    <button onClick={handleSortByName}>Sort by name</button>
+    <button onClick={handleSortByRating}>Sort by rating</button>
+    {/* <button onClick={handleSortByPrice}>Sort by price</button> */}
+    </div>
     <div className=" flex flex-wrap gap-10 justify-center  ">
+    
       {products.map((element)=>{
-        // console.log(element);zZ
         return(
           <div onClick={()=>handleDetailProduct(element)} key={element.asin} className="max-w-[250px] border border-slate-200 p-4 ">
             <img className="w-[250px] max-h-[300px] object-contain" src={element.product_photo} alt="" />
@@ -55,12 +99,12 @@ const SectionProducts = () => {
             <p>Ratings:{element.product_star_rating}</p>
             <p><span>{element.product_price}</span></p>
             <p>{element.coupon_text}</p>
-            
           </div>
         )
       })}
         
     </div>
+    </>
     </>
   )
 }
